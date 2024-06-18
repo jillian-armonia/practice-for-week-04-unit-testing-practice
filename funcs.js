f0ef5d848d8e0576bac0e77733f0d46ab1329d16
Jillian Armonia <jillian.marice@gmail.com>
2024-06-18 15:42:49 +0900

    finished skeleteon functions

--- funcs.js
+++ funcs.js
@@ -1,13 +1,23 @@
 function isFive(num) {
-  // Your code here
+  return num === 5 ? true: false;
 }
 
 function isOdd(number) {
-  // Your code here
+  if (typeof number !== "number"){
+    throw Error("Not a number")
+  }
+
+  return number % 2 !== 0? true: false;
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let range = [];
+
+  for (let i = min; i <= max; i += step){
+    range.push(i);
+  }
+
+  return range;
 }
 
 
