ff64865635055f8b1edd747003b06eda5a31277f
Jillian Armonia <jillian.marice@gmail.com>
2024-06-18 16:02:35 +0900

    finished word class specs

--- test/class-spec.js
+++ test/class-spec.js
@@ -4,31 +4,36 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let newWord;
+
+    beforeEach(() => {
+      newWord = new Word("dinosaur");
+    })
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(newWord).to.have.all.keys('word');
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(newWord.word).to.be.equal("dinosaur");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(newWord.removeVowels()).to.be.equal("dnsr");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(newWord.removeConsonants()).to.be.equal("ioau");;
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      expect(newWord.pigLatin()).to.be.equal("inosaurday");;
     });
   });
 });
